07ed1b6af72375b066898bbd2bd1280dc429552b
nluzinda <natasha.luzinda@gmail.com>
2020-12-27 19:51:22 +0000

    Get user input

--- TripleX.cpp
+++ TripleX.cpp
@@ -16,11 +16,30 @@ int main()
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
 
-    // Print sum and product to the terminal
+    // Print CodeSem and CodeProduct to the terminal
     std::cout << std::endl;
     std::cout << "+ The code is made up of 3 numbers." << std::endl;
     std::cout << "+ The numbers in the code add up to: " << CodeSum << std::endl;
     std::cout << "+ The 3 numbers multiply to give: " << CodeProduct << std::endl;
+
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
+
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    if(GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std::cout << "GREEN LIGHT. You've escaped. Well Done!! ";
+
+    }
+    else
+    {
+        std::cout << "INCORRECT CODE. Try Again."
+    }
+    
 
     return 0;
 }
