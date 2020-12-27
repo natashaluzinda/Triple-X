758ec8707ce6a41baacfe251f162a060e1236605
nluzinda <natasha.luzinda@gmail.com>
2020-12-27 19:23:18 +0000

    Rename variables

--- TripleX.cpp
+++ TripleX.cpp
@@ -6,21 +6,21 @@ int main()
     std::cout << "You are an alien escaping Area 51.";
     // Add new line
     std::cout << std::endl;
-    std::cout << "You need to enter the correct codes to reach your ship..." << std::endl;
+    std::cout << "You need to enter the correct code to reach your ship..." << std::endl;
 
     // Declare 3 different number codes
-    const int a = 4;
-    const int b = 8;
-    const int c = 5;
+    const int CodeA = 4;
+    const int CodeB = 8;
+    const int CodeC = 5;
 
-    const int sum = a + b + c;
-    const int product = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
     // Print sum and product to the terminal
     std::cout << std::endl;
-    std::cout << "The code is made up of 3 numbers." << std::endl;
-    std::cout << "The sum of the code is: " << sum << std::endl;
-    std::cout << "The 3 numbers multiply to give: " << product << std::endl;
+    std::cout << "+ The code is made up of 3 numbers." << std::endl;
+    std::cout << "+ The numbers in the code add up to: " << CodeSum << std::endl;
+    std::cout << "+ The 3 numbers multiply to give: " << CodeProduct << std::endl;
 
     return 0;
 }
