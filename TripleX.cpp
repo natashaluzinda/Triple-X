24da4792cb10c066982cc4016981d83ae39e05e9
nluzinda <natasha.luzinda@gmail.com>
2020-12-27 16:15:42 +0000

    Add comments

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,8 +2,24 @@
 
 int main()
 {
+    // Print welcome messages to the terminal
     std::cout << "You are an alien escaping Area 51.";
+    // Add new line
     std::cout << std::endl;
     std::cout << "You need to enter the correct codes to reach your ship..";
+
+    // Declare 3 different number codes
+    const int a = 4;
+    const int b = 8;
+    const int c = 5;
+
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
+    // Print sum and product to terminal
+    std::cout << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
+
     return 0;
 }
