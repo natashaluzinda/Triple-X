b2029acc1e5995f00bd811eb8d41235a96799315
nluzinda <natasha.luzinda@gmail.com>
2020-12-29 12:52:58 +0000

    Randomise code

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 void PrintIntroduction(int Difficulty)
 {
@@ -12,9 +13,9 @@ bool PlayGame(int Difficulty)
     PrintIntroduction(Difficulty);
 
     // Declare 3 different number codes
-    const int CodeA = 4;
-    const int CodeB = 8;
-    const int CodeC = 5;
+    const int CodeA = rand() % Difficulty + Difficulty;
+    const int CodeB = rand() % Difficulty + Difficulty;
+    const int CodeC = rand() % Difficulty + Difficulty;
 
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
@@ -34,20 +35,24 @@ bool PlayGame(int Difficulty)
     // Check if player guess is correct
     if(GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "\nCORRECT CODE. You've escaped. Well Done!!\n";
+        std::cout << "\n***CORRECT CODE. You've made it past the security on Level " << Difficulty << "***\n";
         return true;
     }
     else
     {
-        std::cout << "\nINCORRECT CODE. Try Again.";
+        std::cout << "\n***INCORRECT CODE. Be Careful and try Again.***";
         return false;
     }
 }
 
 int main()
 {
+    srand(time(NULL)); // Create new random sequence based on time of day
+
     int LevelDifficulty = 1;
-    while(true)
+    const int MaxLevel = 5;
+
+    while(LevelDifficulty <= MaxLevel) // Loop game until all levels are completed
     {
         bool bLevelComplete = PlayGame(LevelDifficulty);
         std::cin.clear(); // Clears any errors
@@ -55,10 +60,10 @@ int main()
 
         if (bLevelComplete)
         {
-            // Increase level difficulty
             ++LevelDifficulty;
         }
         
     }
+    std::cout << "CONGRATULATIONS. YOU'VE REACHED YOUR SHIP!!";
     return 0;
 }
